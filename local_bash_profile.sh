a53c9f028807e059a2ebfe871321a4b9f1ae42c6
Komefumi <komefumi@protonmail.ch>
2021-12-02 23:13:44 +0530

    1. Add load-schema helper to run db:schema:load\n2. Create and use erake helper (short for Could not locate Gemfile or .bundle/ directory)

--- local_bash_profile.sh
+++ local_bash_profile.sh
@@ -1,4 +1,6 @@
-alias migrate="bundle exec rake db:migrate"
+alias erake="bundle exec rake"
+alias migrate="erake db:migrate"
+alias schema-load="erake db:schema:load"
 
 rollback() {
   node exe-scripts/rollback.js $*
